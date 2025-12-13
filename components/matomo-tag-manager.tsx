36b2c0b11189be51181efcc770b290b027e9d607
globalexpat <lugata619@gmail.com>
2025-12-13 05:55:37 +0000

    Fix linting errors in Matomo Tag Manager component

--- components/matomo-tag-manager.tsx
+++ components/matomo-tag-manager.tsx
@@ -7,14 +7,14 @@ export function MatomoTagManager() {
     // Initialize Matomo Tag Manager
     const _mtm = (window._mtm = window._mtm || [])
     _mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' })
-    
+
     const d = document
     const g = d.createElement('script')
     const s = d.getElementsByTagName('script')[0]
-    
+
     g.async = true
     g.src = 'https://matomo.globoexpats.com/js/container_xVOIsphD.js'
-    
+
     if (s && s.parentNode) {
       s.parentNode.insertBefore(g, s)
     }
@@ -26,6 +26,7 @@ export function MatomoTagManager() {
 // Extend Window interface for TypeScript
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     _mtm?: any[]
   }
 }
